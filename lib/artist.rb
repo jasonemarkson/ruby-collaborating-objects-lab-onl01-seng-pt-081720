e3250370b2d7ebb3294481dbc51fd85f36cbb5a5
Jason Markson <jasonmarkson76@gmail.com>
2020-09-14 03:34:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,6 @@ class Artist
   
   def initialize(name)
     @name = name
-    @songs = []
     save
   end
   
@@ -25,7 +24,7 @@ class Artist
 
   
   def add_song(song)
-    @songs << song 
+    song.artist = self 
   end
   
   def self.find_or_create_by_name(name)
@@ -37,6 +36,6 @@ class Artist
   end
   
   def print_songs
-    songs.each { |song| puts song.name }
+    
   end
 end
