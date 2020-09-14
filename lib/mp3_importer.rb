8d3d863cf25b2ad1770b753367fa55f277018b0f
Jason Markson <jasonmarkson76@gmail.com>
2020-09-14 03:41:55 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,10 +6,10 @@ class MP3Importer
   end
   
   def files
-    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+    @files ||= Dir.entries(@path).select { |song| !File.directory?(song) && song.end_with?(".mp3") }
   end
 
   def import
-    files.each {|song| Song.new_by_filename(song)}
+    files.each { |song| Song.new_by_filename(song) }
   end
 end
