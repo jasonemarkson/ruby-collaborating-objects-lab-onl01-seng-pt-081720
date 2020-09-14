613ea729403cbd169384bee76066783f954da2e4
Jason Markson <jasonmarkson76@gmail.com>
2020-09-14 03:44:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,8 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    song = self.new
     song.name = filename.split(" - ")[1]
+    artist.name = filename.split(" - ")[0]
     song 
   end
   
